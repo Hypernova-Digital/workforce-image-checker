50c42f76dc41acc905a69b118c3d0afb71631fc3
Zach Russell <zach@protechig.com>
2023-06-28 12:57:09 -0400

    feat: working but with wrong redirect check

--- workforce-wp-updater/index.js
+++ workforce-wp-updater/index.js
@@ -5,7 +5,7 @@ const btoa = require('btoa');
 const { hideBin } = require('yargs/helpers');
 const sleep = require('util').promisify(setTimeout);
 
-const { dryRun } = yargs(hideBin(process.argv)).argv;
+const { dryRun, postId } = yargs(hideBin(process.argv)).argv;
 
 const WP_API_URL = "https://stgnews.workforce.com/wp-json/wp/v2";
 const WP_USER = "zachrussell";
@@ -13,25 +13,116 @@ const WP_PASSWORD = "CHH5 PDdZ SnVJ Mxw7 LRM3 danW";
 const BASIC_AUTH = "Basic " + btoa(`${WP_USER}:${WP_PASSWORD}`);
 
 let brokenImageCount = 0;
+let fixedImageCount = 0;
 let workingImageCount = 0;
 
 async function checkImage(url) {
   try {
-    const response = await axios.head(url);
+    const response = await axios.head(url, { maxRedirects: 0 });
     if (response.status === 200) {
       workingImageCount++;
+      console.log(`Working image found: ${url}`);
       return true;
     }
   } catch (error) {
-    brokenImageCount++;
-    console.log(`Broken image found: ${url}. Reason: ${error.message}`);
-    return false;
+    if (error.response && error.response.status === 301) {
+      console.log(`Image with redirect found: ${url}`);
+      return false;
+    } else {
+      const urlIncludesOldDomain = url.includes('www.workforce.com') || url.includes('admin.workforce.com') || url.includes('workforce.com');
+      if (urlIncludesOldDomain) {
+        let newUrl = url;
+        newUrl = newUrl.replace('www.workforce.com', 'news.workforce.com');
+        newUrl = newUrl.replace('admin.workforce.com', 'news.workforce.com');
+        newUrl = newUrl.replace(/(?<!www.|news.)workforce.com/g, 'news.workforce.com');
+
+        console.log(`Attempting to fix broken URL: ${url} to ${newUrl}`);
+        try {
+          const fixResponse = await axios.head(newUrl, { maxRedirects: 0 });
+          if (fixResponse.status === 200) {
+            fixedImageCount++;
+            console.log(`Fixed broken URL: ${url} to ${newUrl}`);
+            return newUrl;
+          } else {
+            brokenImageCount++;
+            console.log(`Broken image found: ${newUrl}. Reason: Fix attempt failed`);
+            return false;
+          }
+        } catch (fixError) {
+          brokenImageCount++;
+          console.log(`Broken image found: ${newUrl}. Reason: ${fixError.message}`);
+          return false;
+        }
+      } else {
+        brokenImageCount++;
+        console.log(`Broken image found: ${url}. Reason: ${error.message}`);
+        return false;
+      }
+    }
   }
 }
 
-async function processPage(pageNumber) {
+
+
+async function processSinglePost(postId) {
+  console.log(`------------------- Start processing post ID: ${postId} -------------------`);
   try {
-    console.log(`Processing page: ${pageNumber}`);
+    const res = await axios.get(`${WP_API_URL}/posts/${postId}`);
+    const post = res.data;
+
+    let modifiedContent = post.content.rendered;
+
+    const imgRegex = /<img[^>]*>/gi;
+    const linkWithImgRegex = /<a[^>]*>(.*?<img[^>]*>.*?)<\/a>/gi;
+
+    const imgMatches = modifiedContent.match(imgRegex) || [];
+    const linkWithImgMatches = modifiedContent.match(linkWithImgRegex) || [];
+
+    for (const match of linkWithImgMatches) {
+      const originalUrl = getUrlFromImgTag(match);
+      const newUrl = await checkImage(originalUrl);
+      if (newUrl !== true && newUrl !== false && !dryRun) {
+        modifiedContent = modifiedContent.replace(originalUrl, newUrl);
+        console.log(`Replaced image source in a-tag from ${originalUrl} to ${newUrl}`);
+      } else if (newUrl === false) {
+        modifiedContent = modifiedContent.replace(match, ''); // remove the entire match
+        console.log(`Removed broken image surrounded by an "a" tag: ${originalUrl}`);
+      }
+    }
+
+    for (const match of imgMatches) {
+      const originalUrl = getUrlFromImgTag(match);
+      const newUrl = await checkImage(originalUrl);
+      if (newUrl !== true && newUrl !== false && !dryRun) {
+        modifiedContent = modifiedContent.replace(originalUrl, newUrl);
+        console.log(`Replaced image source from ${originalUrl} to ${newUrl}`);
+      } else if (newUrl === false) {
+        modifiedContent = modifiedContent.replace(match, ''); // remove the entire match
+        console.log(`Removed broken image: ${originalUrl}`);
+      }
+    }
+
+    if (modifiedContent !== post.content.rendered && !dryRun) {
+      const updatedPostResponse = await axios.put(
+        `${WP_API_URL}/posts/${postId}`,
+        { content: modifiedContent },
+        { headers: { Authorization: BASIC_AUTH } }
+      );
+      console.log(`Updated post ID: ${postId}. Full URL: ${updatedPostResponse.data.link}`);
+    }
+
+  } catch (error) {
+    console.error(`Error processing post ${postId}: `, error);
+  }
+  console.log(`------------------- End processing post ID: ${postId} -------------------`);
+}
+
+
+
+async function processPage(pageNumber) {
+  console.log(`========================== Start processing page: ${pageNumber} ==========================`);
+
+  try {
     const res = await axios.get(`${WP_API_URL}/posts?page=${pageNumber}`);
     if (res.data.length === 0) {
       console.log(`No more posts on page: ${pageNumber}`);
@@ -39,35 +130,62 @@ async function processPage(pageNumber) {
     }
 
     for (const post of res.data) {
-      const $ = cheerio.load(post.content.rendered);
-      const imgTags = $("img");
+      console.log(`------------------- Start processing post ID: ${post.id} URL: ${post.link} -------------------`);
+      
+      let modifiedContent = post.content.rendered;
 
-      for (let i = 0; i < imgTags.length; i++) {
-        const imgTag = imgTags[i];
-        const imageUrl = $(imgTag).attr("src");
+      // Regular expressions to find <img> and <IMG> tags
+      const imgRegex = /<img[^>]*>/gi;
+      
+      // Regular expressions to find <a> tags surrounding <img> and <IMG> tags
+      const linkWithImgRegex = /<a[^>]*>(.*?<img[^>]*>.*?)<\/a>/gi;
 
-        if (!(await checkImage(imageUrl)) && !dryRun) {
-          const parent = $(imgTag).parent();
-          if (parent.is("a")) {
-            parent.remove();
-            console.log(`Removed broken image surrounded by an "a" tag: ${imageUrl}`);
-          } else {
-            $(imgTag).remove();
-          }
+      const imgMatches = modifiedContent.match(imgRegex) || [];
+      const linkWithImgMatches = modifiedContent.match(linkWithImgRegex) || [];
 
-          const updatedPostContent = $.html();
-          await axios.put(
-            `${WP_API_URL}/posts/${post.id}`,
-            { content: updatedPostContent },
-            { headers: { Authorization: BASIC_AUTH } }
-          );
-          console.log(`Updated post ID ${post.id}. Full URL: https://stgnews.workforce.com/?p=${post.id}`);
-
-          // Wait for 5 seconds before next request
-          await sleep(5000);
+      // If there's a broken image surrounded by an "a" tag, replace the image URL
+      for (const match of linkWithImgMatches) {
+        const originalUrl = getUrlFromImgTag(match);
+        const newUrl = await checkImage(originalUrl);
+        if (newUrl !== true && newUrl !== false && !dryRun) {
+          modifiedContent = modifiedContent.replace(originalUrl, newUrl);
+          console.log(`Replaced image source in a-tag from ${originalUrl} to ${newUrl}`);
+        } else if (newUrl === false) {
+          modifiedContent = modifiedContent.replace(match, ''); // remove the entire match
+          console.log(`Removed broken image surrounded by an "a" tag: ${originalUrl}`);
         }
       }
+
+      // If there's a broken image tag, replace the image URL
+      for (const match of imgMatches) {
+        const originalUrl = getUrlFromImgTag(match);
+        const newUrl = await checkImage(originalUrl);
+        if (newUrl !== true && newUrl !== false && !dryRun) {
+          modifiedContent = modifiedContent.replace(originalUrl, newUrl);
+          console.log(`Replaced image source from ${originalUrl} to ${newUrl}`);
+        } else if (newUrl === false) {
+          modifiedContent = modifiedContent.replace(match, ''); // remove the entire match
+          console.log(`Removed broken image: ${originalUrl}`);
+        }
+      }
+
+      // Update the post with the modified content
+      if (modifiedContent !== post.content.rendered && !dryRun) {
+        const updatedPostResponse = await axios.put(
+          `${WP_API_URL}/posts/${post.id}`,
+          { content: modifiedContent },
+          { headers: { Authorization: BASIC_AUTH } }
+        );
+        console.log(`Updated post ID: ${post.id}. Full URL: ${updatedPostResponse.data.link}`);
+
+        console.log('Waiting for 30 seconds before processing the next post...');
+        await sleep(.2 * 60 * 1000);
+      }
+
+      console.log(`------------------- End processing post ID: ${post.id} URL: ${post.link} -------------------`);
     }
+
+    console.log(`========================== End processing page: ${pageNumber} ==========================`);
 
     return true;
   } catch (error) {
@@ -75,14 +193,29 @@ async function processPage(pageNumber) {
   }
 }
 
+function getUrlFromImgTag(imgTag) {
+  const srcMatch = imgTag.match(/src="([^"]*)"/i);  // using 'i' flag to make it case insensitive
+  return srcMatch ? srcMatch[1] : '';
+}
+
+
 async function processPosts() {
-  let page = 1;
-  while (await processPage(page)) {
-    page++;
+  console.log('=============== Start processing posts ===============');
+  
+  if (postId) {
+    await processSinglePost(postId);
+  } else {
+    let page = 1;
+    while (await processPage(page)) {
+      page++;
+    }
   }
 
   console.log(`Total broken images: ${brokenImageCount}`);
+  console.log(`Total fixed images: ${fixedImageCount}`);
   console.log(`Total working images: ${workingImageCount}`);
+
+  console.log('=============== End processing posts ===============');
 }
 
 processPosts();
